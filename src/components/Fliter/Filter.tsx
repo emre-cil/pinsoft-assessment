a1e8fcc1c583c39ba3b8c8e28660e5af44185286
Emre Çil <65237072+emre-cil@users.noreply.github.com>
2023-12-23 09:40:27 +0300

    style: fix problems

--- src/components/Fliter/Filter.tsx
+++ src/components/Fliter/Filter.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import styles from './Filter.module.scss';
 import categories from '@/data/categories.json';
 import { CategoryType } from '@/types/types';
+import { Icon } from '@iconify/react';
 interface FilterProps {
   selectedCategory: string;
   setSelectedCategory: (category: string) => void;
 }
 
 const Filter: React.FC<FilterProps> = ({ selectedCategory, setSelectedCategory }) => {
+  const [isOpen, setIsOpen] = React.useState(window.innerWidth > 1024);
+
   const handleClick = (category: string) => {
     if (selectedCategory === category) {
       setSelectedCategory('');
@@ -17,10 +20,26 @@ const Filter: React.FC<FilterProps> = ({ selectedCategory, setSelectedCategory }
   };
 
   return (
-    <div className={styles.container}>
-      <label htmlFor="categoryFilter" className={styles.categoryLabel}>
-        Filter
-      </label>
+    <div
+      className={styles.container}
+      style={{
+        height: isOpen ? '250px' : '44px',
+        transition: 'height 0.3s ease-in-out',
+      }}
+    >
+      <div className={styles.categoryTitle}>
+        <label htmlFor="categoryFilter" className={styles.categoryLabel}>
+          Filter
+        </label>
+        <Icon
+          icon="mingcute:down-line"
+          style={{
+            transform: isOpen ? 'rotate(180deg)' : 'rotate(0deg)',
+            transition: 'transform 0.3s ease-in-out',
+          }}
+          onClick={() => setIsOpen(!isOpen)}
+        />
+      </div>
       <ul className={styles.categoryList}>
         {categories.map((category: CategoryType) => (
           <li
